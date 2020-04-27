da1cbcbd57153f45aa96d3a89b78b851317e063c
Surapong007 <58117195+Surapong007@users.noreply.github.com>
2020-04-27 19:20:15 +0700

    ui

--- src/core/Interface.java
+++ src/core/Interface.java
@@ -5,6 +5,7 @@
  */
 package core;
 
+import java.util.Scanner;
 import model.Equipment;
 import model.EquipmentGrade;
 import utility.factory.DungeonFactory;
@@ -15,44 +16,82 @@ import utility.factory.MinionFactory;
  * @author SURAPONGCHAMALAI
  */
 public class Interface {
-    
-    PlayerProfile profile;
-    
-    public void goBattle() {
-        Dungeon dun = DungeonFactory.createDungeon(DungeonFactory.HARD_INDEX);
-        dun.enter(profile);
-    }
-    
-    public void upequipmentlevel() {
 
-        
+    Scanner Scan = new Scanner(System.in);
+    PlayerProfile profile;
+
+    public void goBattle() {
+
+        System.out.println("Choose Difficulty");
+        System.out.println("1 : Easy");
+        System.out.println("2 : Medium");
+        System.out.println("3 : Hard");
+        int num = Scan.nextInt();
+
+        System.out.println("Difficult = " + num);
+        if (num == 1) {
+            DungeonFactory.createDungeon(DungeonFactory.EASY_INDEX);
+        } else if (num == 2) {
+            DungeonFactory.createDungeon(DungeonFactory.MEDIUM_INDEX);
+        } else if (num == 3) {
+            DungeonFactory.createDungeon(DungeonFactory.HARD_INDEX);
+        } else {
+            System.out.println("You choose wrong difficulty");
+        }
+
+        // Dungeon dun = DungeonFactory.createDungeon(DungeonFactory.HARD_INDEX);
+        // dun.enter(profile);
     }
-    
-    
-    
-    
+
+    public void upequipmentlevel() {
+        System.out.println("Select Your Equipment");
+        System.out.println("1 = Weapon");
+        System.out.println("2 = Armor");
+        int num = Scan.nextInt();
+        
+            if (num == 1) {
+                System.out.println("Equipment = Weapon " );
+            }if(num == 2){
+                System.out.println("Equipment = Armor " );
+            }else{
+                System.out.println("Wrong input");
+            }
+        }
+   
+
     public static void main(String[] args) {
         Interface n = new Interface();
-        n.localLoadSaveInterface();
+        //n.localLoadSaveInterface();
+        // n.goBattle()
+        n.upequipmentlevel();
     }
-    
-    private void saveLocalProfile(int slot) {        
+
+    private void saveLocalProfile(int slot) {
         LocalSaveSystem.saveProfile(profile, slot);
     }
-    
+
     public void localLoadSaveInterface() {
+
+        System.out.println("Choose the Slot to Save 0 - 2");
+        int num = Scan.nextInt();
+        System.out.println("You save to slot " + num);
+        for (int i = 0; i < 4; i++) {
+            if (i == num) {
+                LocalSaveSystem.saveProfile(profile, num);
+            }
+        }
         LocalSaveSystem savesys = new LocalSaveSystem();
         PlayerProfile[] slots = savesys.getSaveSlots();
         StringBuilder s = new StringBuilder();
-            for (int i = 0; i < slots.length; i++) {
+        for (int i = 0; i < slots.length; i++) {
             PlayerProfile slot = slots[i];
-            if ( slot != null ) {
+            if (slot != null) {
                 s.append("SLOT ").append(i).append(" :").append(slot.getUsername()).append("\n");
-            }else {
+            } else {
                 s.append("SLOT ").append(i).append(" :").append(" emptyslot \n");
             }
         }
         System.out.println(s);
     }
-     
+
 }
