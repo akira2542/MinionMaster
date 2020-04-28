a6808c49a8765ae09b802762b63dfafa0829ccd6
Surapong007 <58117195+Surapong007@users.noreply.github.com>
2020-04-28 22:40:44 +0700

    interface

--- src/core/Interface.java
+++ src/core/Interface.java
@@ -20,6 +20,44 @@ public class Interface {
     Scanner Scan = new Scanner(System.in);
     PlayerProfile profile;
 
+ //  newgame loadgame
+    //create profile createchar
+    public void newGame(){
+        
+           
+        System.out.println("Load or Create new save game");
+        System.out.println("Press 1 to load local");
+        System.out.println("Press 2 to load cloud");
+        System.out.println("Press 3 to create new");
+        
+       int num = Scan.nextInt();
+      
+       if(num == 1){
+           System.out.println("Select slot to load local 0 - 2");
+            int load = Scan.nextInt();
+           for (int i = 0; i < 3; i++) {
+               if(load == i){
+                   System.out.println(" Load Profile on slot" + i );
+                   
+               }
+           }
+       }  if(num == 2){
+           System.out.println("Insert your Username");
+           Scanner String = new Scanner(System.in);
+           String user = String.nextLine();
+           System.out.println("Username = " + user);
+       }
+       if(num == 3){
+           System.out.println("Select slot to New game 0 - 2");
+            int create = Scan.nextInt();
+           for (int i = 0; i < 3; i++) {
+               if(create == i){
+                   System.out.println("Create New save game on slot " + i);
+               }
+           }
+       }
+     
+    }
     public void goBattle() {
 
         System.out.println("Choose Difficulty");
@@ -39,8 +77,8 @@ public class Interface {
             System.out.println("You choose wrong difficulty");
         }
 
-        // Dungeon dun = DungeonFactory.createDungeon(DungeonFactory.HARD_INDEX);
-        // dun.enter(profile);
+         //Dungeon dun = DungeonFactory.createDungeon(DungeonFactory.HARD_INDEX);
+        //dun.enter(profile);
     }
 
     public void upequipmentlevel() {
@@ -52,10 +90,11 @@ public class Interface {
             if (num == 1) {
                 System.out.println("Equipment = Weapon " );
                 Equipment.getIncreaseEquipmentLevelPrice(e.getWeaponlv(), e.getWeaponGrade());
+                
             }if(num == 2){
                 System.out.println("Equipment = Armor " );
                 Equipment.getIncreaseEquipmentLevelPrice(e.getArmorlv(), e.getArmorGrade());
-            }else{
+            }else if (num<1||num>2){
                 System.out.println("Wrong input");
             }
            
@@ -65,9 +104,10 @@ public class Interface {
 
     public static void main(String[] args) {
         Interface n = new Interface();
-        //n.localLoadSaveInterface();
-        // n.goBattle()
-        n.upequipmentlevel();
+       // n.localLoadSaveInterface();
+       //  n.goBattle();
+       n.newGame();
+             // n.upequipmentlevel();
     }
 
     private void saveLocalProfile(int slot) {
