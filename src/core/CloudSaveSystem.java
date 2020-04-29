488c6c6ab0fb41c61f6878f8244b1ddfc20403ba
CL <60165701+chananananu@users.noreply.github.com>
2020-04-30 03:53:58 +0700

    Update CloudSaveSystem.java

--- src/core/CloudSaveSystem.java
+++ src/core/CloudSaveSystem.java
@@ -1,4 +1,4 @@
-/*
+                         /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -19,7 +19,7 @@ import model.Equipment;
 import model.Minion;
 import model.PlayableMinion;
 import utility.JDBC.DBConnector;
-import utility.factory.MinionFactory;
+import utility.factory.MinionFactory;     
 
 /**
  *
